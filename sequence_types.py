f973b597890f37ff2a78cae8dab8f15260d30630
Morgan Spencer (s5711229) <Your Github email>
2024-10-17 17:38:01 +0100

    last change

--- sequence_types.py
+++ sequence_types.py
@@ -1,3 +1,16 @@
 my_list = [200, 15, "Programming", 1+2, "Lab 3"]
 print(len(my_list))
 my_list.append(7)
+my_list.insert(0,"computing")
+my_list.remove(7)
+len(my_list)
+print(my_list[0])
+my_list[1] = 100
+print(type(my_list))
+my_tuple = tuple(my_list)
+print(type(my_tuple))
+print(my_tuple[0]) 
+print(len(my_tuple))
+another_tuple = (5,13,24)
+joined_tuple = my_tuple + another_tuple
+print(joined_tuple)
